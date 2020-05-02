35e6e511f6407c517641d65064309fcb000acb22
riverandeye <riverandeye@gmail.com>
2020-05-03 00:27:51 +0900

    add constants

---
+++ src/constant.ts
@@ -0,0 +1,16 @@
+export const BREAKPOINT = 640;
+
+export const THEME = {
+  topDark: "#190013",
+  topPurple: "#240419",
+  topCircle: "#3c0f23",
+  body: "#cfa076",
+  bottom: "#d7ad7d",
+};
+
+export const FONT_COLOR = {
+  date: "#e46a4a",
+  place: "#d04232",
+  theme: "#a61a1d",
+  personnel: "#a01828",
+};
