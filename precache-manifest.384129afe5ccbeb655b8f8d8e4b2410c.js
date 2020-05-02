cc58a7703fafdae56c5fcf43766e557cc726c0a0
riverandeye <riverandeye@gmail.com>
2020-05-03 00:33:59 +0900

    Updates

---
+++ precache-manifest.384129afe5ccbeb655b8f8d8e4b2410c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544a3831a1729a69019edffd0fbfcceb",
+    "url": "/game-jam/index.html"
+  },
+  {
+    "revision": "9f1fca537554b8ecf5e4",
+    "url": "/game-jam/static/js/2.733a888e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-jam/static/js/2.733a888e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd21c8f4835e3a8eb55",
+    "url": "/game-jam/static/js/main.c96aa6d0.chunk.js"
+  },
+  {
+    "revision": "1edaa2351816878d5040",
+    "url": "/game-jam/static/js/runtime-main.2e99c035.js"
+  }
+]);
