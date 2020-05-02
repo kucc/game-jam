1a50e7eaa49d5538f78b3571d9b54f58356725b2
riverandeye <riverandeye@gmail.com>
2020-05-03 00:27:34 +0900

    add layout

---
+++ src/component/Layout/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import * as S from "./styles";
+import Header from "../header";
+import Footer from "../footer";
+
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <S.Layout>
+      <Header />
+      <S.Content>{children}</S.Content>
+      <Footer />
+    </S.Layout>
+  );
+};
+
+export default Layout;
